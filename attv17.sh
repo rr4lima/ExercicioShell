5df34831cffea7ee9a4be77e80be75200d10b063
Rafa <165973120+rr4lima@users.noreply.github.com>
2026-05-29 16:29:21 -0300

    Create attv17.sh

---
+++ attv17.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+while true
+do
+    clear
+
+    echo "Relógio digital"
+    echo ""
+
+    echo "Data atual: $(date +%d/%m/%Y)"
+    echo "Horário atual: $(date +%H:%M:%S)"
+
+    sleep 1
+done
